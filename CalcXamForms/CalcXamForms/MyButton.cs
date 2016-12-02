17747b6c16c54cbef9162e36accb778a8aae9b4e
kaby76 <kenneth.domino@domemtech.com>
2016-12-02 08:14:32 -0500

    Buttons changed to be derived from "MyButton" as there is no "label template".

---
+++ CalcXamForms/CalcXamForms/MyButton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CalcXamForms
+{
+    public class MyButton : Label
+    {
+        public MyButton()
+        {
+            base.Margin = new Thickness(1,1,1,1);
+            TapGestureRecognizer gestureRecognizer = new TapGestureRecognizer();
+            gestureRecognizer.Tapped += (s, e) => {
+                if (Command != null && Command.CanExecute(null))
+                {
+                    Command.Execute(null);
+                }
+            };
+            this.GestureRecognizers.Add(gestureRecognizer);
+        }
+
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create<MyButton, ICommand>(x => x.Command, null);
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        private FormattedString _text;
+        public string Fstext
+        {
+            get
+            {
+                return _text.ToString();
+            }
+            set
+            {
+                FormattedString fs = new FormattedString();
+                fs.Spans.Add(
+                    new Span()
+                    {
+                        Text = value,
+                        BackgroundColor = Color.Gray,
+                        ForegroundColor = Color.White,
+                        FontSize = 30
+                     });
+                _text = fs;
+                base.FormattedText = fs;
+                base.HorizontalTextAlignment = TextAlignment.Center;
+                base.VerticalTextAlignment = TextAlignment.Center;
+            }
+        }
+    }
+}
---
+++ CalcXamForms/CalcXamForms/View1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CalcXamForms
+{
+    public enum CameraOptions
+    {
+        Front,
+        Rear
+    };
+
+    public class CameraPreview : View
+    {
+        public static readonly BindableProperty CameraProperty = BindableProperty.Create(
+          propertyName: "Camera",
+          returnType: typeof(CameraOptions),
+          declaringType: typeof(CameraPreview),
+          defaultValue: CameraOptions.Rear);
+
+        public CameraOptions Camera
+        {
+            get { return (CameraOptions)GetValue(CameraProperty); }
+            set { SetValue(CameraProperty, value); }
+        }
+    }
+}
